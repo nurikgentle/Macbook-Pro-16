2546e36a33acb22b3ca13e3fc5d4b36f68f1e105
nurikgentle <102224735+nurikgentle@users.noreply.github.com>
2022-04-21 00:58:42 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let errorParagragh = document.getElementById("error")
+console.log(errorParagragh)
+
+function purchase() {
+     console.log("button clicked")
+     errorParagragh.textContent = "Пхахахахаахххааа, Заскамили Мамонта Как Ты 😂"
+}
